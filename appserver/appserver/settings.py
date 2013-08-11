3a20bc1c255e64ce5396044da76347a4bff0f7ce
Min Seong Kang <spiritbanner@gmail.com>
2013-08-10 21:20:26 -1000

    Changed postgresql setting again.

--- appserver/appserver/settings.py
+++ appserver/appserver/settings.py
@@ -20,7 +20,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'prj_4k',   # Or path to database file if using sqlite3.
+        'NAME': 'proj_4k',   # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'projadmin',
         'PASSWORD': 'projadmin',
