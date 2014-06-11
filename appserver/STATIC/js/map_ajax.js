6dfeeaa889c78f6f1a831742a293d3e42d67a1b0
Junwon Lee <esanzy87@gmail.com>
2014-06-11 21:25:46 +0900

    add ajax feature
    yet returns 401 error

---
+++ appserver/STATIC/js/map_ajax.js
@@ -0,0 +1,18 @@
+function getMapData()
+{
+	$.ajax({
+		type: "GET"
+		, url: "/api/0.1/location"
+		, dataType: "JSON"
+		, beforSend: function(xhr)
+		{
+			xhr.setRequestHeader("Content-Type","application/json");
+			xhr.setRequestHeader("Accept","application/json");
+			xhr.setRequestHeader("Authorization", "ApiKey tester:1a2b3c4d5e");
+		}
+		, success: function(data)
+		{
+			alert(data);
+		}
+	});
+}
--- appserver/STATIC/js/map_script.js
+++ appserver/STATIC/js/map_script.js
@@ -135,4 +135,6 @@ $(document).ready(function()
 {
 	initialize();
 	getLocations();
+	
+	getMapData();
 });
